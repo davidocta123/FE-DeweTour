9a11b0512103aa29cd6739eed69c8cc803f6f6ce
davidocta123 <davi20269ti@student.nurulfikri.ac.id>
2023-06-12 08:23:52 -0700

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from '../component/Home'
-// import Container from 'react-bootstrap/container';
+import { Container } from 'react-bootstrap';
 
 function Index() {
     return (
-      // <Container>
+      <Container>
         <Home/>
-      // </Container>
+      </Container>
     );
   }
   
